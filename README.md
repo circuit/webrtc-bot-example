1dd6d75f0573575a82b8c33abcbe3ef3d468826a
Roger Urscheler <roger.urscheler@unify.com>
2017-09-20 21:23:15 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# webrtc-bot-example
+
+[Electron](https://electron.atom.io/)-based Bot utilizing the WebRTC capabilities of the [Circuit JS SDK](https://github.com/circuit/circuit-sdk).
+
+> Electron is based on node.js and Chromium and is therefore able to utilize the Circuit JS WebRTC APIs unlike a regular node.js app.
+
+## Scenarios covered in this example:
+### Greet participants by name when joining a conference
+* Using `callStatus` event listen for participants joining
+* Use [IBM Text to Speech](https://www.ibm.com/watson/services/text-to-speech/) service to generate audio buffer for greeting
+* Use the [AudioContext]((https://developer.mozilla.org/en-US/docs/Web/API/AudioContext)) of the Web Audio API to convert the audio buffer to a MediaStream
+* Use the new [`setAudioVideoStream`](https://circuitsandbox.net/sdk/classes/Client.html#method_setAudioVideoStream) API to transmit the greeting to the conference
+
+### Transcribe the conference and post to the conversation
+* Todo
+* Use https://cloud.google.com/speech/ or https://www.ibm.com/watson/services/speech-to-text/
+
+
+## Getting Started
+
+* [Register an account](https://www.circuit.com/web/developers/registration) on circuitsandbox.net (if you didn't yet)
+* [Register a bot](http://circuit.github.io/oauth) on the sandbox (OAuth 2.0 Client Credentials)
+
+### Run the app
+
+```bash
+    git clone https://github.com/circuit/webrtc-bot-example.git
+    cd webrtc-bot-example
+    npm install
+    npm start
+```
---
+++ renderer.js
@@ -0,0 +1,112 @@
+// Limitations: The bot can only be in a single call at a time. Multiple
+// client crendentials app would be needed to be in multiple calls at
+// the same time.
+
+const activeCalls = {};
+
+let client = new Circuit.Client({
+  client_id: '235aff91bef7412f83f1f3fa0af07693',
+  client_secret: '65dd12552c004526a90bfbe6db2e856e',
+  domain: 'circuitsandbox.net',
+  autoRenewToken: true
+});
+
+function welcomeParticipant(callId, participant) {
+  let url = 'https://watson-api-explorer.mybluemix.net/text-to-speech/api/v1/synthesize?';
+  let params = Circuit.Utils.toQS({
+    accept: 'audio/ogg;codecs=opus',
+    voice: 'en-US_MichaelVoice',
+    text: `Hello ${participant.firstName}, glad you could make the call.`
+  });
+
+  fetch(`${url}?${params}`)
+    .then(res => res.arrayBuffer())
+    .then(buffer => {
+      var audioCtx = new AudioContext();
+      var source = audioCtx.createBufferSource();
+      var output = audioCtx.createMediaStreamDestination();
+
+      // Decode the audio data
+      audioCtx.decodeAudioData(buffer, decodedData => {
+        // Set the AudioContext's source and connect the source to the output
+        source.buffer = decodedData;
+        source.connect(output);
+
+        // Set the audio/video stream to be sent and then start the stream
+        client.setAudioVideoStream(callId, output.stream)
+          .then(() => source.start(0));
+
+        /* for debugging the stream can be recorded and shown with a player on the UI
+        var chunks = [];
+        var mediaRecorder = new MediaRecorder(output.stream);
+        mediaRecorder.ondataavailable = evt => chunks.push(evt.data);
+        mediaRecorder.onstop = evt =>
+          document.querySelector('audio').src = URL.createObjectURL(new Blob(chunks, {'type':'audio/ogg;codecs=opus'}));
+        mediaRecorder.start();
+        setTimeout(() => mediaRecorder.stop(), 3000);
+        */
+      });
+    })
+    .catch(console.error)
+}
+
+// Check if the item indicates this is a new conversation for the bot
+function isNewConversation(item) {
+  return (item.type === Circuit.Enums.ConversationItemType.SYSTEM &&
+    (item.system.type === Circuit.Enums.SystemItemType.PARTICIPANT_ADDED ||
+    item.system.type === Circuit.Enums.SystemItemType.CONVERSATION_CREATED) &&
+    item.system.affectedParticipants.includes(client.loggedOnUser.userId));
+}
+
+// Event Listeners
+client.addEventListener('itemAdded', evt => {
+  let item = evt.item;
+  if (isNewConversation(item)) {
+    console.log(`Bot is added to a conversation: ${item.convId}`);
+    client.addTextItem(item.convId, 'Thanks for adding me to the conversation.');
+  }
+});
+
+client.addEventListener('callEnded', evt => {
+  // Remove call from activeCalls list
+  delete activeCalls[evt.call.callId];
+});
+
+client.addEventListener('callStatus', evt => {
+  let call = evt.call;
+
+  // New conference has started, join the call
+  if (evt.reason === 'callStateChanged' && call.state === Circuit.Enums.CallStateName.Started) {
+    // Don't join the call if bot had joined earlier
+    if (!activeCalls[call.callId]) {
+      // Remember this active call so bot does not start it again after everyone
+      // left the call. This is because 'joinConference' does also starts the conference.
+      activeCalls[call.callId] = true;
+
+      client.joinConference(call.callId)
+        .then(() => console.log(`Joined call: ${call.callId}`));
+    }
+    return;
+  }
+
+  // Last participant left. Leave as well.
+  if (evt.reason === 'participantRemoved' && !call.participants.length) {
+    client.leaveConference(evt.call.callId)
+      .then(() => console.log(`Left call: ${evt.call.callId}`));
+    return;
+  }
+
+  // New participant joined, welcome him/her
+  if (evt.reason === 'participantAdded') {
+    console.log(`New participant on call: ${evt.participant.firstName} ${evt.participant.lastName}`);
+    welcomeParticipant(call.callId, evt.participant);
+    return;
+  }
+});
+
+// Print all events for debugging
+Circuit.supportedEvents.forEach(e => client.addEventListener(e, console.log));
+
+// Initialization
+client.logon()
+  .then(user => console.log(`Logged on as bot: ${user.emailAddress}`))
